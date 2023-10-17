e3d781fcfa39b27aea656f90448e3b781542b6b9
testlucik <testluciktest@gmail.com>
2023-10-18 01:50:17 +0300

    Implemented the DriverManager

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,30 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+
+        //New Window Code
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://www.google.com/");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://www.tekwill.md/");
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+        driver.quit();
+        System.out.println("The execution is over");
+
+
     }
 }
